0f2b268bfd17969a693bd83ead14cb77e2cb519c
Kghbln <karsten@hoffmeyer.info>
2016-02-12 00:16:01 +0100

    Make v1.3.1 release
    * Switched to consistently use __DIR__
    * Added license file
    * Added license information
    
    Change-Id: I249d26bb220cebab1b5855ef99d0c4f10d9a0233

--- MagicNoCache.php
+++ MagicNoCache.php
@@ -12,7 +12,7 @@
  * @author Kimon Andreou (Kimon)
  * @author Pavel Astakhov (Pastakhov)
  * @copyright (C) 2007 Kimon Andreou
- * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ * @license https://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
 
 // Check to see if we are being called as an extension or directly
@@ -24,28 +24,26 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 $wgExtensionCredits['parserhook'][] = array(
 	'path'           => __FILE__,
 	'name'           => 'MagicNoCache',
-	'version'        => '1.3.0',
+	'version'        => '1.3.1',
 	'url'            => 'https://www.mediawiki.org/wiki/Extension:MagicNoCache',
 	'author'         => array(
 		'Kimon Andreou',
 		'[https://www.mediawiki.org/wiki/User:Pastakhov Pavel Astakhov]',
 		'...'
 		),
-	'descriptionmsg' => 'magicnocache-desc'
+	'descriptionmsg' => 'magicnocache-desc',
+	'license-name' => 'GPL-2.0+'
 );
-
-// Tell the whereabouts of files
-$dir = __DIR__;
 
 // Allow translations for this extension
 $wgMessagesDirs['MagicNoCache'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['MagicNoCache'] = $dir . '/MagicNoCache.i18n.php';
-$wgExtensionMessagesFiles['MagicNoCacheMagic'] = $dir . '/MagicNoCache.i18n.magic.php';
+$wgExtensionMessagesFiles['MagicNoCache'] = __DIR__ . '/MagicNoCache.i18n.php';
+$wgExtensionMessagesFiles['MagicNoCacheMagic'] = __DIR__ . '/MagicNoCache.i18n.magic.php';
 
 // Check to see if we have the magic word in the article
 $wgHooks['InternalParseBeforeLinks'][] = function( &$parser, &$text ) {
 	global $wgOut, $wgAction;
-	$mw = MagicWord::get('MAG_NOCACHE');
+	$mw = MagicWord::get( 'MAG_NOCACHE' );
 
 	// if it is there, remove it and disable caching
 	if ( !in_array( $wgAction, array( 'submit', 'edit') ) && $mw->matchAndRemove($text) ) {
